0d5bd9bcd652c11b695fde16b5d678b911272abe
jesusp301081 <jesusp301081@gmail.com>
2022-03-30 21:02:09 -0300

    inicio del projecto

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+
+function uno(){
+    console.log(1);
+};
+
+let dos = function(){
+    console.log(2);
+};
+
+const tres = ()=>{
+ console.log(3);
+}
+
+const obj = {
+
+    uno: 1,
+    dos: 2,
+    tres: 3
+}
+
+class Nom{
+
+}
