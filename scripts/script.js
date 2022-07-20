72a259064b45f1e4deffa281add9002b142e80af
jesusp301081 <jesusp301081@gmail.com>
2022-07-19 20:05:24 -0500

    project complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,48 @@
+let energy = 3;
+let round = 1;
 
-function uno(){
-    console.log(1);
-};
+let newEnergy = document.getElementById('energy');
 
-let dos = function(){
-    console.log(2);
-};
+let newRound = document.getElementById('round');
 
-const tres = ()=>{
- console.log(3);
-}
+let resetButton = document.getElementById('button-reset');
 
-const obj = {
+let newRoundButton = document.getElementById('button-new-round');
 
-    uno: 1,
-    dos: 2,
-    tres: 3
-}
+let plusEnergy = document.getElementById('button-plus-energy');
 
-class Nom{
+let lessEnergy = document.getElementById('button-negative-energy');
 
-}
+/************************************plus energy button**********************************/
+plusEnergy.addEventListener('click', ()=>{
+    energy = energy+1
+    newEnergy.innerHTML = energy
+    if (energy >= 10) {
+        energy = 10;
+        newEnergy.innerHTML = energy;
+    };
+;});
+/*************************************minus energy button**************************************************/
+lessEnergy.addEventListener('click', ()=>{
+    energy = energy-1;
+    newEnergy.innerHTML = energy;
+    if (energy <= 0) {
+        energy = 0;
+        newEnergy.innerHTML = energy;
+    };
+});
+/************************************new round button*****************************************/
+newRoundButton.addEventListener('click', ()=>{
+    round = round+1
+    newRound.innerHTML = round
+    energy = energy+2
+    newEnergy.innerHTML = energy
+    if(energy >= 10){
+        energy = 10
+        newEnergy.innerHTML = energy
+    }
+});
+/************************************* reset button *********************************************/
+resetButton.addEventListener('click', ()=>{
+    location.reload();
+});
